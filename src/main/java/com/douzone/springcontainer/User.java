cf40a495918ba30ea261362092998d8b32b230a7
BIT <BIT@DESKTOP-4M0FKCM>
2019-03-04 10:39:26 +0900

    init commit

---
+++ src/main/java/com/douzone/springcontainer/User.java
@@ -0,0 +1,13 @@
+package com.douzone.springcontainer;
+
+public class User {
+	private String name = "안대혁";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
